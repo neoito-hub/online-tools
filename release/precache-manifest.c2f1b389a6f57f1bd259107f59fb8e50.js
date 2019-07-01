eb7ec746b95b44922fa5228dcf9a0488af0d0f66
safaldas <safaldas.g@neoito.com>
2019-07-01 12:44:13 +0530

    build

---
+++ release/precache-manifest.c2f1b389a6f57f1bd259107f59fb8e50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb5b16403dfd2586a21d2090b763eed3",
+    "url": "/static/media/wrench-logo.fb5b1640.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e5b123948be62363933e",
+    "url": "/static/js/main.e5b12394.chunk.js"
+  },
+  {
+    "revision": "2c3e9e95922d677317e1",
+    "url": "/static/js/1.2c3e9e95.chunk.js"
+  },
+  {
+    "revision": "e5b123948be62363933e",
+    "url": "/static/css/main.bee697e5.chunk.css"
+  },
+  {
+    "revision": "2c3e9e95922d677317e1",
+    "url": "/static/css/1.16289d2a.chunk.css"
+  },
+  {
+    "revision": "132908bdc5935f4952c0d65b1076ac08",
+    "url": "/index.html"
+  }
+];
