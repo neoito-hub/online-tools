ba8faeb2d0e98cb7dc5779866e637e3b7fd840dc
safaldas <safaldas.g@neoito.com>
2020-07-31 15:01:31 +0530

    Add(nav): Edit in github icon

--- components/layout/MainNavbar/MainNavbar.js
+++ components/layout/MainNavbar/MainNavbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import { Container, Navbar } from "shards-react";
+import { Container, Navbar, NavItem, DropdownItem } from "shards-react";
 
 import NavbarToggle from "./NavbarToggle";
 
@@ -26,6 +26,18 @@ const MainNavbar = ({ layout, stickyTop }) => {
       <Container className="p-0">
         <Navbar type="light" className="align-items-stretch flex-md-nowrap p-0">
           <NavbarToggle />
+          <NavItem>
+            <DropdownItem
+              href={`https://github.com/neoito-hub/online-tools/blob/master/pages/${
+                document.URL.split("/")[document.URL.split("/").length - 1]
+              }.js`}
+              target="_blank"
+            >
+              <span className="d-none d-md-inline-block">
+                <i className="material-icons">&#xE150;</i>Edit in github
+              </span>
+            </DropdownItem>
+          </NavItem>
         </Navbar>
       </Container>
     </div>
@@ -40,11 +52,11 @@ MainNavbar.propTypes = {
   /**
    * Whether the main navbar is sticky to the top, or not.
    */
-  stickyTop: PropTypes.bool
+  stickyTop: PropTypes.bool,
 };
 
 MainNavbar.defaultProps = {
-  stickyTop: true
+  stickyTop: true,
 };
 
 export default MainNavbar;
